dfaa8220271120a84c21099033f376b548a0db41
Nícalo Ribeiro <nicalo63@gmail.com>
2022-02-05 16:12:55 -0300

    Funcao para desenhar celulas com suas paredes.
    Co-authored-by: Joao Vitor <joaovitordmrosa@hotmail.com>

--- grafos1-hotspot/src/App.js
+++ grafos1-hotspot/src/App.js
@@ -13,6 +13,33 @@ function App() {
   function Cell(column, line, p5) {
     this.column = column
     this.line = line
+    this.walls = [true, true, true, true] // CIMA, DIREITA, BAIXO, ESQUERDA
+    this.visited = false
+
+    this.showCell = function() {
+      let x = this.column * cellWidth;
+      let y = this.line * cellWidth;
+      p5.stroke(212, 241, 244);
+      if (this.walls[0]) {
+        p5.line(x, y, x + cellWidth, y);
+      }
+      if (this.walls[1]) {
+        p5.line(x + cellWidth, y, x + cellWidth, y + cellWidth);
+      }
+      if (this.walls[2]) {
+        p5.line(x + cellWidth, y + cellWidth, x, y + cellWidth);
+      }
+      if (this.walls[3]) {
+        p5.line(x, y + cellWidth, x, y);
+      }
+  
+      if (this.visited) {
+        p5.noStroke();
+        p5.fill(24, 154, 180, 100);
+        p5.rect(x, y, cellWidth, cellWidth);
+      }
+    }
+    
   }
 
   const setup = (p5, parentRef) =>{
@@ -21,10 +48,23 @@ function App() {
 
     totalColumns = p5.floor(p5.width / cellWidth)
     totalRows = p5.floor(p5.height / cellWidth)
+
+    for (let j = 0; j < totalRows; j++) {
+      for (let i = 0; i < totalColumns; i++) {
+        var cell = new Cell(i, j, p5)
+        arrayOfCells.push(cell)
+      }
+    }
+
+    current = arrayOfCells[0]
   }
 
   const draw = (p5) => {
     p5.background(5, 68, 94)
+
+    for (let i = 0; i < arrayOfCells.length; i++) {
+      arrayOfCells[i].showCell();
+    }
   }
 
   return (
